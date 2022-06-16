d906753c24c6cabb33c5c70270fd008f3c2cd5fc
hisa883 <hisa1412@icloud.com>
2022-06-16 14:36:19 +0900

    add-dash-boards

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'users/dash_boards'
    # rootをログイン画面に設定
   devise_scope :user do
     root "users/sessions#new"
-    get 'dash_boards', to: 'users/dashboars'
+    
   end
 
   devise_for :users, :controllers => {
